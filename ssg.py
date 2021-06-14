1e278129b6d379cdb5106bd743c3e66a58191464
Tomas Harcar <tomasharcar@gmail.com>
2021-06-14 16:14:14 +0200

    fix1

--- ssg.py
+++ ssg.py
@@ -10,4 +10,4 @@ def main(source='content', dest='dist'):
     Site(**config).build()
 
 
-typer.run(main())
+typer.run(main)
--- ssg/site.py
+++ ssg/site.py
@@ -8,7 +8,7 @@ class Site:
         self.dest = Path(dest)
 
     def create_dir(self, path):
-        directory = self.dest + '//' + path.relative_to(self.source)
+        directory = self.dest / path.relative_to(self.source)
         directory.mkdir(parents=True, exists_ok=True)
 
     def build(self):
